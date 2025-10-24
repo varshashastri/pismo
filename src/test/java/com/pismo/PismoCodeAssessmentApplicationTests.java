7800e9fc3a37afb3921706a855b95bcd954769a9
Varsha Shastri <varsha.shastri@lendscape.com>
2025-10-24 15:29:25 +0100

    trial

---
+++ src/test/java/com/pismo/PismoCodeAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PismoCodeAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
