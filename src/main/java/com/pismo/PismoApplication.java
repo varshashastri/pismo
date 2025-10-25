2ec4fc11b769ac59d1a2b073c9d72a373a524796
Varsha Shastri <varsha.shastri@lendscape.com>
2025-10-25 11:13:58 +0100

    removed unwanted changes

--- src/main/java/com/pismo/PismoApplication.java
+++ src/main/java/com/pismo/PismoApplication.java
@@ -1,23 +1,11 @@
 package com.pismo;
 
-import com.pismo.entity.OperationType;
-import com.pismo.repository.OperationTypeRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.transaction.annotation.Transactional;
-
 @SpringBootApplication
 public class PismoApplication {
-	private final OperationTypeRepository operationTypeRepository;
-
-	public PismoApplication(OperationTypeRepository operationTypeRepository) {
-		this.operationTypeRepository = operationTypeRepository;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(PismoApplication.class, args);
 	}
-
 }
